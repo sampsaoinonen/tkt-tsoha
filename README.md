fe2b6e90f40eeb1f21b33c6c6aa43a47a7e8c5cf
sampsaoinonen <80003556+sampsaoinonen@users.noreply.github.com>
2021-11-21 23:07:10 +0200

    välipalautus 2

--- README.md
+++ README.md
@@ -11,3 +11,34 @@ Sovelluksen avulla voidaan tarkastella erilaisia tilastoja NHL-jääkiekkoilijoi
 * Ylläpitäjä voi kirjautua sisään sekä ulos. 
 * Ylläpitäjä voi moderoida keskustelua.
 * Sovellus hakee datan jääkiekkoilijoista sivustolta moneypuck.com CSV-muodossa
+
+
+
+## Välipalautus 2
+
+#### Toteutuneet toiminnot
+
+* CSV-muodossa oleva data tallentuu players-tauluun
+* Uudelleenkäynnistäessä olemassa oleva data ei tuplaannu
+* Aloitussivu hakee datan sivulle
+* Yksittäisiä pelaajia voi klilata, jolloin pääsee muuttuvalla id:llä tehtyyn pelaajan omaan sivuun
+* Pelaajan sivulla näkyy pelajan data, kommentit pelaajasta ja kommenttikenttä alhaaalla
+* Pelaaja sivun data tallentuu comments-tauluun, joka on liitetty players-tauluun id:llä
+
+#### Vastaan tulleet hankaluudet
+
+* Datan hakeminen suoraan nettiosoitteesta ei onnistunut monen yrityksen jälkeen. Tarvitsen tähän apua.
+
+#### TODO seuraavaksi
+
+* Kirjautumistoiminnot
+* Turvallisuus
+* Statistiikan järjestely eri attribuuteilla
+* Timestamp kommentteihin sekä kommentoijan nimi (kommentointi vain kirjautumalla?)
+* Ohjelman jäsentely(tosin tässä vaiheessa koin helpommaksi kaiken olevan yhdessä tiedostossa)
+
+#### Kehitysideoita
+
+* Sovellus voisi ehkä olla universaali pallo- tai urheilulajeihin, jossa aloitussivulla käyttäjä voisi laittaa CSV-tiedoston tai linkin, jonka perusteella data haetaan.
+
+
---
+++ app.py
@@ -0,0 +1,53 @@
+import csv
+from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")   
+db = SQLAlchemy(app)
+
+with open("nhl-stats.csv") as stats:                #here data from already downloaded csv-file 
+    for row in csv.reader(stats, delimiter=","):    #is being parsed into sql-table        
+        if(row[0] == "\ufeff" or row[0] == "Player Name"):  #first lines with other than player data is not included
+           continue
+
+        result = db.session.execute("SELECT COUNT(*) FROM players WHERE name=:name AND team=:team", {"name":row[0], "team":row[1]}) #This is for updating data. If playerdata exists it is only being updated not inserted
+        if(result.fetchone()[0] == 1):           
+            continue    #Missing sql update here
+
+        sql =  "INSERT INTO players (name, team, pos, games, goals, assists, points, plusminus, penalty, shots, gwg, ppg, ppa, shg, sha, hits, blocked) VALUES (:name, :team , :pos, :games, :goals, :assists, :points, :plusminus, :penalty, :shots, :gwg, :ppg, :ppa, :shg, :sha, :hits, :blocked)"
+        to_insert = {"name":row[0], "team":row[1], "pos":row[2], "games":row[3], "goals":row[4], "assists":row[5], "points":row[6], "plusminus":row[7],  #
+        "penalty": row[8], "shots":row[9], "gwg":row[10], "ppg":row[11], "ppa":row[12], "shg":row[13], "sha":row[14], "hits":row[15], "blocked":row[16]}
+        db.session.execute(sql, to_insert)
+db.session.commit()
+
+
+@app.route("/")     
+def index():
+    result = db.session.execute("SELECT * FROM players")
+    players = result.fetchall()
+    return render_template("index.html", players=players)
+
+@app.route("/<int:id>")
+def page(id):
+    result_player = db.session.execute("SELECT * FROM players WHERE id=:id", {"id":id})
+    player = result_player.fetchone()
+    result_comments = db.session.execute("SELECT * FROM comments WHERE players_id=:id", {"id":id})
+    comments = result_comments.fetchall()
+
+    return render_template("player.html", player=player, comments=comments)
+    
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    players_id = request.form["id"]
+    sql = "INSERT INTO comments (content, players_id) VALUES (:content, :players_id)"
+    db.session.execute(sql, {"content":content, "players_id":players_id})
+    db.session.commit()        
+    print(players_id)
+    return redirect("/" + players_id)
+
+
+#return redirect("/<int:request.form.id>")
